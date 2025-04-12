0ff47248b0dcab2109cd5f6267c0242d575dfa8e
keyboardmunji <wjdwngus12155@naver.com>
2025-04-12 23:42:19 +0900

    Update BOJ-25966.cpp

---
+++ BOJ/BOJ-27323.cpp
@@ -0,0 +1,18 @@
+// Authored by : keyboardmunji
+// Created on 2025-04-12
+#include <bits/stdc++.h>
+using namespace std;
+
+int a,b;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> a >> b;
+    cout << a *b;
+    return 0;
+}
+
+
+

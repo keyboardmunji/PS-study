8f359801aaf4a15ac8eb73a5a0f14733ca6d3717
keyboardmunji <wjdwngus12155@naver.com>
2025-05-27 09:05:53 +0900

    Update BOJ-15439 .cpp

---
+++ BOJ/BOJ-15439.cpp
@@ -0,0 +1,20 @@
+// Authored by : keyboardmunji
+// Created on 2025-05-27.
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+
+int main(void) {
+    ios :: sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n;
+    cout << n * (n-1);
+
+
+    return 0;
+}
+/*
+1.
+*/

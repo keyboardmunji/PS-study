6af33da6973a4317c7ff738d4e5bfa4d3366e9d0
keyboardmunji <wjdwngus12155@naver.com>
2025-05-28 23:40:04 +0900

    Update BOJ-33871 .cpp

---
+++ BOJ/BOJ-33871.cpp
@@ -0,0 +1,19 @@
+// Authored by : keyboardmunji
+// Created on 2025-05-28.
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int n;
+int main(){
+
+    ios :: sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n;
+    if(n%2){
+        cout << "Soomin";
+    }else
+        cout << "Song";
+    return 0;
+}

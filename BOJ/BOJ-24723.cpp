172f7a9e3e489f97a7febc5cf76ee1f58822c502
keyboardmunji <wjdwngus12155@naver.com>
2025-06-01 23:17:07 +0900

    Update BOJ-24723 .cpp

---
+++ BOJ/BOJ-24723.cpp
@@ -0,0 +1,18 @@
+// Authored by : keyboardmunji
+// Created on 2025-06-01.
+#include <bits/stdc++.h>
+using namespace std;
+
+long long h,w,one,nine;
+
+int main(void) {
+    ios :: sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    cout << (1 << n);
+}
+/*
+1. 2의 n승 구하기.
+*/

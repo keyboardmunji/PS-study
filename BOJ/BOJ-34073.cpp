2409a2513db4aa4246bf0b974809912a3cd07cfe
keyboardmunji <wjdwngus12155@naver.com>
2025-07-31 16:32:45 +0900

    Update BOJ-34073.cpp

---
+++ BOJ/BOJ-34073.cpp
@@ -0,0 +1,22 @@
+// Authored by : keyboardmunji
+// Created on 2025-07-31.
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(void) {
+    ios :: sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    string x;
+    cin >> n;
+    while (n--) {
+        cin >> x;
+        cout << x << "DORO ";
+    }
+}
+/*
+1. DORO
+    - 바빠서 간단하게 푸는 문제.
+*/

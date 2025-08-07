de0c8fcc3384a5b51a08e785285e2a94b9e04113
keyboardmunji <wjdwngus12155@naver.com>
2025-08-07 22:16:12 +0900

    Update BOJ-2420.cpp

---
+++ BOJ/BOJ-2420.cpp
@@ -0,0 +1,16 @@
+// Authored by : keyboardmunji
+// Created on 2025-08-07.
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios :: sync_with_stdio(false);
+    cin.tie(NULL);
+
+    long long a, b;
+    cin >> a >> b;
+    cout << abs(a-b);
+}
+/*
+1. 사파리 월드
+*/

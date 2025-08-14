2428c8887e3b47284fe09ee24966e91415e7c7e9
keyboardmunji <wjdwngus12155@naver.com>
2025-08-14 22:41:19 +0900

    Update BOJ-10718.cpp

---
+++ BOJ/BOJ-10718.cpp
@@ -0,0 +1,14 @@
+// Authored by : keyboardmunji
+// Created on 2025-08-14.
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios :: sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cout << "강한친구 대한육군\n강한친구 대한육군";
+}
+/*
+1. We love kriii
+*/

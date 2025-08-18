91a94db601d8a3e83c53613dbbe3c588c56662b0
keyboardmunji <wjdwngus12155@naver.com>
2025-08-19 00:13:27 +0900

    Update BOJ-5717.cpp

---
+++ BOJ/BOJ-5717.cpp
@@ -0,0 +1,20 @@
+// Authored by : keyboardmunji
+// Created on 2025-08-19.
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios :: sync_with_stdio(false);
+    cin.tie(NULL);
+
+    while (1) {
+        int a,b;
+        cin >> a >> b;
+        if (a == 0 && b == 0)
+            return 0;
+        cout << a + b <<'\n';
+    }
+}
+/*
+1. 상근이의 친구들
+*/

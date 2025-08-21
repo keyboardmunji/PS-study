e71f608b63ba113ab57f207287e0deec90f5193a
keyboardmunji <wjdwngus12155@naver.com>
2025-08-22 01:35:13 +0900

    Update BOJ-15733.cpp

---
+++ BOJ/BOJ-15733.cpp
@@ -0,0 +1,13 @@
+// Authored by : keyboardmunji
+// Created on 2025-08-22.
+#include <bits/stdc++.h>
+using namespace std;
+int main(void) {
+    ios :: sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cout << "I'm Sexy";
+}
+/*
+1. 나는 누구인가
+*/

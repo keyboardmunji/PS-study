8d9f8608887e741b6505576630d47cec873204fc
keyboardmunji <wjdwngus12155@naver.com>
2025-08-24 03:44:35 +0900

    Update BOJ-7891.cpp

---
+++ BOJ/BOJ-7891.cpp
@@ -0,0 +1,19 @@
+// Authored by : keyboardmunji
+// Created on 2025-08-24.
+#include <bits/stdc++.h>
+using namespace std;
+int main(void) {
+    ios :: sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int a,b;
+        cin >> a>> b;
+        cout << a + b << '\n';
+    }
+}
+/*
+1. Can You add this
+*/

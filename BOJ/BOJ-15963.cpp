34dc4a6698f4961ccc3d4eb4c6394e4a8da4d9b7
keyboardmunji <wjdwngus12155@naver.com>
2025-09-01 01:27:08 +0900

    Update BOJ-15963.cpp

---
+++ BOJ/BOJ-15963.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    long long a,b;
+    cin >> a >> b;
+    cout << (a == b ? 1 : 0);
+
+    return 0;
+}
